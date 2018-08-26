5249dbd106bdbf67a5343490ba05b50ad79e63a5
Todd Hainsworth <hainsworth.todd@gmail.com>
2018-08-26 16:14:43 +0930

    initial structure

---
+++ api.go
@@ -0,0 +1,7 @@
+package sectionio
+
+import "fmt"
+
+const version = "v1"
+
+var apiBaseURL = fmt.Sprintf("https://aperture.section.io/api/%s", version)
---
+++ structs.go
@@ -0,0 +1,34 @@
+package sectionio
+
+// Account represents a Section.IO Account object
+type Account struct{}
+
+// Application represents a Section.IO Application object
+type Application struct{}
+
+// Billing represents a Section.IO Billing object
+type Billing struct{}
+
+// Domain represents a Section.IO Domain object
+type Domain struct{}
+
+// Environment represents a Section.IO Environment object
+type Environment struct{}
+
+// Help represents a Section.IO Help object
+type Help struct{}
+
+// OutagePage represents a Section.IO OutagePage object
+type OutagePage struct{}
+
+// Proxy represents a Section.IO Proxy object
+type Proxy struct{}
+
+// Synthetics represents a Section.IO Synthetics object
+type Synthetics struct{}
+
+// User represents a Section.IO User object
+type User struct{}
+
+// Zone represents a Section.IO Zone object
+type Zone struct{}
